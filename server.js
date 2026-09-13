0d6a03b582c7c33e8acf9b94e988101dbcd24219
Ryan Eakin <RyanEakin@protonmail.com>
2026-09-12 18:27:27 -0600

    finished-team-activity--commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { testConnection } from './src/models/db.js';
 import { getAllOrganizations } from './src/models/organizations.js';
+import { getAllProjects } from './src/models/projects.js';
 import path from 'path';
 
 
@@ -58,8 +59,10 @@ app.get('/organizations', async (req, res) => {
 
 
 app.get('/projects', async (req, res) => {
+    const projects = await getAllProjects();
     const title = 'Service Projects';
-    res.render('projects', { title });
+
+    res.render('projects', { title, projects });
 });
 
 app.get('/categories', async (req, res) => {
