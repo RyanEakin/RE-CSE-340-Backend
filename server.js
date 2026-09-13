90494a974106a7569b9a0977c405b9caed98f54d
Ryan Eakin <RyanEakin@protonmail.com>
2026-09-12 18:32:14 -0600

    seeing-if-it-is-test-Connections-fault-commit

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/categories', async (req, res) => {
 
 app.listen(PORT, async () => {
   try {
-    await testConnection();
+    //await testConnection();
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
   } catch (error) {
