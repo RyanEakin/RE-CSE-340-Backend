f867736b023ec76a58d2032ab910944e7f978bc0
Ryan Eakin <RyanEakin@protonmail.com>
2026-09-15 21:44:48 -0600

    first-commit-setting-up-files

--- src/models/projects.js
+++ src/models/projects.js
@@ -2,7 +2,7 @@ import db from './db.js'
 
 const getAllProjects = async() => {
     const query = `
-        SELECT Project_Id, p.title, o.name, p.description, location, TO_CHAR(project_date, 'DD-MM-YYY') AS project_date, p.organization_id
+        SELECT Project_Id, p.title, o.name, p.description, location, TO_CHAR(project_date, 'DD-MM-YYYY') AS project_date, p.organization_id
       FROM public.projects AS p LEFT JOIN public.organizations AS o ON p.organization_id = o.organization_id;
     `;
 
